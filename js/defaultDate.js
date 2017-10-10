24e4482617a04c9defb7796d6c639a250eb47b16
Steve Oliver <steve@circatree.com>
2017-10-10 15:23:54 -0700

    Use US format.

--- js/defaultDate.js
+++ js/defaultDate.js
@@ -20,7 +20,7 @@
         var regexp = /[0-9]{4}-[0-9]{2}-[0-9]{2}/;
         if (val.match(regexp)) {
           parts = val.split('-');
-          $el.val(parts[2] + '/' + parts[1] + '/' + parts[0]);
+          $el.val(parts[1] + '/' + parts[2] + '/' + parts[0]);
         }
         var attributes = ['min', 'max'], i, field;
         for (i in attributes) {
@@ -29,7 +29,7 @@
             val = $el.attr(field);
             if (val && val.match(regexp)) {
               parts = val.split('-');
-              $el.attr(field, parts[2] + '/' + parts[1] + '/' + parts[0]);
+              $el.attr(field, parts[1] + '/' + parts[2] + '/' + parts[0]);
             }
           }
         }
